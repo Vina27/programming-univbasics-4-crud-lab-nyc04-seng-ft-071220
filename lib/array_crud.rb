6f9741ee165904f98bf6d6c8f8d8c2fe15c511c8
Maysoon Tabali <mtab03@me.com>
2020-07-16 15:50:12 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,7 +8,7 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  array = ["bob", "john", "mary", "tom"]
+  array = ["wow", "I", "am", "really", "learning"]
   element.push("cat")
 end
 
